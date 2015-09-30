024aac32971d67841d8b4f65439d2418ac6df559
irogers97 <irogers97@hotmail.com>
2015-09-30 19:55:38 +0100

    Initial version
    Signed-off-by: irogers97 <irogers97@hotmail.com>

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/eclipse/workspace/opencart/catalog/');
+define('DIR_SYSTEM', '/opt/eclipse/workspace/opencart/system/');
+define('DIR_LANGUAGE', '/opt/eclipse/workspace/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/opt/eclipse/workspace/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/opt/eclipse/workspace/opencart/system/config/');
+define('DIR_IMAGE', '/opt/eclipse/workspace/opencart/image/');
+define('DIR_CACHE', '/opt/eclipse/workspace/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/eclipse/workspace/opencart/system/storage/download/');
+define('DIR_LOGS', '/opt/eclipse/workspace/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/eclipse/workspace/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/eclipse/workspace/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'res0lved');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
